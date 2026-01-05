5f6901d6587cc4af850fb8f8fc1a2b9eeb1775b3
flashBNK <lolmaks23@gmail.com>
2026-01-05 11:22:17 +0300

    add init, main, run

---
+++ backend/app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from .config import settings
+from .database import init_db
+from .routes import products_router, categories_router, cart_router
+
+app = FastAPI(
+    title=settings.app_name,
+    debug=settings.debug,
+    docs_url='/api/docs',
+    redoc_url='/api/redoc',
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = settings.cors_origins,
+    allow_credential=True,
+    allow_metods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount('/static', StaticFiles(directory=settings.static_dir), name='static')
+
+@app.get('/')
+def root():
+    return{
+        'message': 'Welcome to fastapi shop API',
+        'docs': 'api/docs',
+    }
